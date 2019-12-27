46952cbe120c4bea68471e1a12c91a01e32aa8a5
Samuel <sam99dave@gmail.com>
2019-12-27 22:39:06 +0530

    Transaction Function Added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,38 @@
 function Blockchain() {
   this.chain = []; // blocks for mining
-  this.newTransactions = []; // Transactions before mining
+  this.pendingTransactions = []; // Transactions before mining
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce, // comes from proof of work
     hash: hash,
     previousBlockHash: previousBlockHash
   };
 
-  this.newTransactions = []; // clearing transactions for the next block
+  this.pendingTransactions = []; // clearing transactions for the next block
   this.chain.push(newBlock);
 
   return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function() {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, receiver) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    receiver: receiver
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()["index"] + 1;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -4,4 +4,8 @@ const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(123421, "sgfa4436#", "sdah4766");
 
+bitcoin.createNewTransaction(2000, "Sam", "Simon");
+
+bitcoin.createNewBlock(123421, "sgfa4436#", "sdah4766");
+
 console.log(bitcoin);
